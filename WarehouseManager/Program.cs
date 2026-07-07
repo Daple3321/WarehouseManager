a227dbe5405396703282b713ac45b4e22cf06f56
Egor <egorchik.kobylkin@gmail.com>
2026-07-07 22:25:30 +0300

    Map / to scalar api, always run migrations and scalar api

--- WarehouseManager/Program.cs
+++ WarehouseManager/Program.cs
@@ -45,14 +45,16 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    using var scope = app.Services.CreateScope();
-    await scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.MigrateAsync();
+using var scope = app.Services.CreateScope();
+await scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.MigrateAsync();
 
-    app.MapOpenApi();
-    app.MapScalarApiReference(); 
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.MapGet("/", () => Results.Redirect("/scalar/v1"));
+
+// if (app.Environment.IsDevelopment())
+// {
+// }
 
 app.UseExceptionHandler();
 app.UseMiddleware<LoggingMiddleware>();
